917f6d5bd05bef150c795407a0e532f78e3b2c73
alpha <ssongjinseob@gmail.com>
2025-03-05 09:33:04 -0500

    d

--- src/bert/model.py
+++ src/bert/model.py
@@ -1,9 +1,21 @@
-import pandas as pd
-import scipy
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import pipeline
+import torch.nn as nn
 
-
+class Finbert(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone',num_labels=3)
+        self.tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
+        self.nlp = pipeline("sentiment-analysis", model=self.finbert, tokenizer=self.tokenizer)
+    def infernece(self, text : list[str], enablePrint=False):
+        '''
+        sentences = ["there is a shortage of capital, and we need extra financing",  
+                    "growth is strong and we have plenty of liquidity", 
+                    "there are doubts about our finances", 
+                    "profits are flat",
+                    "It's positive and negative at the sametime"]
+        '''
+        results = self.nlp(text)
+        if (enablePrint): print(results)
+        return results
--- src/webscraped.py
+++ src/webscraped.py
@@ -8,7 +8,7 @@ except:
     print("You need to have API_KEY for newsapi.org in .env")
 import os
 
-def get_latest_news(company_name, n=10, duration=14):
+def news1(company_name, n=10, duration=14):
     """
         company_name : Apple, Meta, etc
         n: Number of articles getting.
@@ -17,9 +17,10 @@ def get_latest_news(company_name, n=10, duration=14):
     api_key = os.getenv("API_KEY")
     
     past = (datetime.utcnow() - timedelta(days=duration)).strftime('%Y-%m-%dT%H:%M:%SZ')
-    current = (datetime.utcnow()).strftime('%Y-%m-%dT%H:%M:%SZ')
+    current = (datetime.utcnow() - timedelta(days=2)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
     
-    query = f'{company_name} company'
+    query = f"{company_name} company"
     url = f'https://newsapi.org/v2/everything?q={query}&from={past}&to={current}&sortBy=publishedAt&language=en&pageSize={n}&apiKey={api_key}'
     
     response = requests.get(url)
@@ -29,13 +30,40 @@ def get_latest_news(company_name, n=10, duration=14):
     news_data = response.json()
     if (news_data['status'] != 'ok'):
         raise Exception("Perhaps Token Limit!")
-
+    process = [o for o in news_data['articles'][1:] if (company_name in o['content'] or company_name in o['description'] or company_name.lower() in o['description'] or company_name.lower() in o['content'])]
+    
     text_infos = {
         'company_name' : query,
         # also 'articles'
-        'articles': news_data['articles'][1:]
+        'articles': process
     }
     return text_infos
-if __name__ == "__main__":
-    company_name = input("Enter the company name: ")
-    get_latest_news(company_name)
+
+
+
+
+from pygooglenews import GoogleNews
+import pandas as pd
+from datetime import date, timedelta
+
+def news2(company_name):
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    now = today.strftime("%m/%d/%Y")
+    yesterday = yesterday.strftime("%m/%d/%Y")
+
+    googlenews = GoogleNews(start=yesterday, end=now)
+    googlenews.search(company_name)
+    result = googlenews.result()
+    df = pd.DataFrame(result)
+    return df
+
+if __name__ == '__main__':
+    company_name = input("Please provide the name of the Company or a Ticker: ")
+    if company_name:
+        print(f"Searching for and analyzing {company_name}, please be patient, it might take a while...")
+        news_df = news2(company_name)
+        print(news_df)
+    else:
+        print("No company name provided.")
+        
